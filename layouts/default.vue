05e03c46a33d31d8ec1e077e68eba0107fa42415
lwolodkiewicz <l.wolodkiewicz@systemycallcenter.pl>
2020-10-27 22:58:47 +0100

    Added messenger

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
+    <div id="fb-root"></div>
+    <div class="fb-customerchat" attribution="setup_tool" page_id="103764398106758" theme_color="#67b868"></div>
     <Header />
     <ColorModePicker />
     <nuxt />
@@ -14,6 +16,23 @@ export default {
   components: {
     Header,
     ColorModePicker
+  },
+  mounted() {
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml: true,
+        version: 'v8.0'
+      })
+    }
+    ;(function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) return
+      js = d.createElement(s)
+      js.id = id
+      js.src = 'https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    })(document, 'script', 'facebook-jssdk')
   }
 }
 </script>
