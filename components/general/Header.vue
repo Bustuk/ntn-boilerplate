e3628c93292b6c66a70b22c69a6af87dc0665f16
lwolodkiewicz <l.wolodkiewicz@systemycallcenter.pl>
2020-10-28 00:30:56 +0100

    testing mobile hamburger menu

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="">
-    <ul class="flex fixed top-0 inset-x-0 p-3 h-24 header">
+    <ul class="hidden md:flex fixed top-0 inset-x-0 p-3 h-24 header">
       <li class="flex-1 flex-grow mr-2">
         <img src="../../static/img/gad-logo.png" class="h-20 w-28" alt="" />
       </li>
@@ -10,10 +10,10 @@
       <li class="flex-1 flex-grow-0 mr-2 text-big flex items-center link">
         <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
       </li>
-      <li class="flex-1 flex-grow-0 flex items-center link">
+      <li class="flex-1 flex-grow-0 mr-2 flex items-center link">
         <nuxt-link class="btn block" to="/projects">O nas</nuxt-link>
       </li>
-      <li class="flex-1 flex-grow-0 flex items-center link relative">
+      <li class="flex-1 ml-2 flex-grow-0 flex items-center link relative">
         <SocialIcon :link="''" :icon="'youtube'" />
       </li>
       <li class="flex-1 flex-grow-0 flex items-center link relative">
@@ -23,6 +23,17 @@
         <SocialIcon :link="''" :icon="'instagram'" />
       </li>
     </ul>
+    <ul class="flex md:hidden fixed top-0 inset-x-0 p-3 h-24 header">
+      <li class="flex-1 flex-grow mr-2">
+        <img src="../../static/img/gad-logo.png" class="h-20 w-28" alt="" />
+      </li>
+      <li @click="toggleMenuVisiblity" class="flex-1 flex-grow-0 mr-2 text-big flex items-center link">
+        <a class="btn block">Menu</a>
+      </li>
+    </ul>
+    <!-- <div v-bind:class="{ hidden: !menuVisible }" class="w-full h-full fixed top-auto left-0 bg-white opacity-90 z-50">
+      menu
+    </div> -->
   </nav>
 </template>
 
@@ -31,13 +42,27 @@ import SocialIcon from './SocialIcon'
 
 export default {
   name: 'Header',
+  data() {
+    return {
+      menuVisible: false
+    }
+  },
   components: {
     SocialIcon
+  },
+  methods: {
+    toggleMenuVisiblity() {
+      this.menuVisible = !this.menuVisible
+    }
   }
 }
 </script>
 
 <style lang="postcss" scoped>
+.menu-visible {
+  display: flex !important;
+}
+
 .link {
   min-width: max-content;
 }
